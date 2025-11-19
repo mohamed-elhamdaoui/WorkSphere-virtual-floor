cf927847efd5b66ffc0b62ff3a683ef15c4d06fc
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2025-11-19 23:37:55 +0100

    feat : add save employe functionality

--- main.js
+++ main.js
@@ -2,26 +2,41 @@ let addWorker = document.getElementById("addWorker");
 let popup = document.getElementById("popup")
 let close = document.getElementById("close")
 
+let fullName = document.getElementById("fullName");
+let Role = document.getElementById("Role");
+let photoURL = document.getElementById("photoURL")
+// console.log(photoURL)
+let Email = document.getElementById("Email")
+let phone = document.getElementById("phone")
 
-addWorker.addEventListener("click", () => {
+let imgInput = document.getElementById("imgInput")
+
+let empCardContainer = document.getElementById("empCardContainer")
+
+function openPopup() {
     popup.classList.remove("hidden")
     popup.parentElement.classList.remove("hidden")
 
-    // document.querySelector("main").classList.add("bg-black")
-})
+    photoURL.addEventListener("input", () => {
 
-close.addEventListener("click", () => {
+        // console.log(photoURL.value)
+        if (photoURL.value.trim() === "") {
+            imgInput.src = "Image-not-found.png"
+
+        } else {
+            imgInput.src = photoURL.value;
+        }
+        console.log(imgInput.src)
+    })
+}
+
+function closePopup() {
     popup.classList.add("hidden")
     popup.parentElement.classList.add("hidden")
-
-})
-
-console.log()
-
+}
 
 // let addExperience = document.getElementById("addExperience");
 let cardPlaces = document.getElementById("cardPlaces");
-
 
 function addExperience() {
     // cardPlaces.innerHTML += `<div class="grid grid-cols-2 grid-rows-1 gap-x-3 gap-y-1 p-2 bg-gray-100 border rounded-md mx-2">
@@ -79,8 +94,41 @@ function addExperience() {
 
         expBloc.remove()
     })
+}
 
+function saveEmploye() {
+
+    let nom = fullName.value;
+    let rule = Role.value;
+    let mail = Email.value;
+    let telephone = phone.value;
+
+    let empCard = `<div class="bg-white shadow rounded-md p-2 border flex gap-1 items-center">
+                        <div class="border-2  w-[60px]  h-[60px] rounded-full overflow-hidden">
+                            <img class="w-full h-full object-cover" src="${imgInput.src}" alt="">
+                        </div>
+                        <div class="border-2 ">
+                            <h3 class="font-semibold">${nom} </h3>
+                            <p class="text-sm text-gray-600">${rule}.</p>
+                        </div>
+
+                    </div>`
+
+    empCardContainer.insertAdjacentHTML("afterbegin", empCard)
+    closePopup()
 
 }
 
 
+
+// let x = 55 ;
+
+// delete x ;
+
+// function sum(a,a)
+// {
+//     console.log(a + a);
+// }
+
+
+// sum(2,5);
